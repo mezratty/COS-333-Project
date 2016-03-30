7052fb6ac89049cdf9948a5987f5126f1ce2ed63
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-03-30 00:37:39 -0400

    first commit of calendar

---
+++ TestCal/TestCal/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TestCal
+//
+//  Created by Alexa Wojak on 3/28/16.
+//  Copyright (c) 2016 Alexa Wojak. All rights reserved.
+//
+
+import UIKit
+import PDTSimpleCalendar
+class ViewController: UIViewController {
+    
+    //let tomorrow = NSDate(timeInterval: 60*60*24, sinceDate: NSDate())
+   // var selected = didSelectDate(tomorrow)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = .yellowColor()
+        
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    /*func didSelectDate(date:NSDate)
+    {
+        view.backgroundColor = .greenColor()
+        self.title = "TestCal2"
+        //  if (cv.selectedDate == date) {
+        //self.window!.backgroundColor = UIColor.greenColor();
+        // }
+        //NSLog(@"Date Selected : %@",date);
+        //NSLog(@"Date Selected with Locale %@", [date descriptionWithLocale:[NSLocale systemLocale]]);
+    }
+
+*/
+}
+
