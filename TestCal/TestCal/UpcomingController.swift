e6855b370c601c07aec61672cf5851a57d5ac7c6
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-03-31 16:23:25 -0400

    TableView

--- TestCal/TestCal/UpcomingController.swift
+++ TestCal/TestCal/UpcomingController.swift
@@ -8,21 +8,58 @@
 
 
 import UIKit
-class UpcomingController: UIViewController {
- 
+class UpcomingController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView  =   UITableView()
+    
+    var items: [String] = ["BODYHYPE SHOW", "SOCCER GAME"]
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        view.backgroundColor = .greenColor()
+        
+        tableView.frame         =   CGRectMake(0, 50, 320, 200);
+        tableView.delegate      =   self
+        tableView.dataSource    =   self
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.addSubview(tableView)
+        
         
         // Do any additional setup after loading the view, typically from a nib.
       
     }
     
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
     
 }
 
