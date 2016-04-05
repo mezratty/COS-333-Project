b4f6129ff6c17db8cbdd9fdbf6dfd95e4bb2036a
Divya Mehta <divyamehta@nat-oitwireless-inside-vapornet100-c-31305.Princeton.EDU>
2016-04-04 22:09:43 -0400

    new project

---
+++ 333 Project/333 Project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  333 Project
+//
+//  Created by Divya Mehta on 4/4/16.
+//  Copyright © 2016 Divya Mehta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
