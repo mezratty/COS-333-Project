20a79d711bf0dce35d932e730f4f574e707be14b
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-04-07 16:07:19 -0400

    html reader

--- 333 Project/Pods/Target Support Files/Pods-333 Project/Pods-333 Project-frameworks.sh
+++ 333 Project/Pods/Target Support Files/Pods-333 Project/Pods-333 Project-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-333 Project/Alamofire.framework"
+  install_framework "Pods-333 Project/HTMLReader.framework"
   install_framework "Pods-333 Project/PDTSimpleCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-333 Project/Alamofire.framework"
+  install_framework "Pods-333 Project/HTMLReader.framework"
   install_framework "Pods-333 Project/PDTSimpleCalendar.framework"
 fi
