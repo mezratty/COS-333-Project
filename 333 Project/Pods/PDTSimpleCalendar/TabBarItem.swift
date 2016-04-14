f756191a9608a38acad35ae5ede46b9c220e2b66
Mitch Hamburger <MitchHamburger@nat-tvwna-inside-visitornet101-b-1115.princeton.org>
2016-04-14 15:24:18 -0400

    for harry from mitch with love

---
+++ 333 Project/Pods/PDTSimpleCalendar/TabBarItem.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarItem.swift
+//  Pods
+//
+//  Created by Mitch Hamburger on 4/14/16.
+//
+//
+
+import UIKit
+
+@IBDesignable
+
+class PushButtonView: UITabBarItem {
+    @IBInspectable var fillColor: UIColor = UIColor.greenColor()
+    @IBInspectable var isCalendar: Bool = true
+    
+    
+}
+
