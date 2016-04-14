ec2863342c1719f04944201866c8f7fa2d7f8d5a
Mitch Hamburger <MitchHamburger@nat-tvwna-inside-visitornet101-b-1115.princeton.org>
2016-04-14 16:14:34 -0400

    d

--- 333 Project/333 Project/DrawUpcoming.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-let size = CGSize(width: 120, height: 200)
-
-UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-let context = UIGraphicsGetCurrentContext()
