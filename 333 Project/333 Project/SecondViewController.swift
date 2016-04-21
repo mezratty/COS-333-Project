fd3010af6970fe268455e1a1de3b0105c963770b
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-04-21 17:40:22 -0400

    table

--- 333 Project/333 Project/SecondViewController.swift
+++ 333 Project/333 Project/SecondViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Divya Mehta. All rights reserved.
 //
 
-var events = [NSMutableArray]()
+
 
 import UIKit
 import Firebase
@@ -14,6 +14,9 @@ import Firebase
 class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tableView: UITableView  =   UITableView()
+    var events = NSMutableArray()
+    //var events = [NSMutableArray]()
+    //var sections: NSArray = NSArray.init(array: [2, 200])
     
     // Get a reference to our posts
     var ref = Firebase(url:"https://blistering-torch-3510.firebaseio.com/events")
@@ -30,29 +33,50 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 print(error.description)
         })
         
+        var counter = 0
+        var month = 4
+        var day = 1
+        var year = 2016
+        for var i = 0; i < 372; i+=1 {
+            if (counter < 31) {day += 1; counter++}
+            else {counter = 0; day = 1; month+=1; if(month == 13){month = 1; year+=1}}
+            
+            var totalDate = month*1000000+day*10000+year
+            
+            print(totalDate)
         
-        ref.queryOrderedByChild("dateInt").observeEventType(.Value, withBlock: {snapshot in
+        //ref.queryOrderedByChild("dateInt").observeEventType(.Value, withBlock: {snapshot in
+        ref.queryOrderedByChild("dateInt").queryEqualToValue(totalDate).observeEventType(.Value, withBlock: {snapshot in
             var tempEvents = [NSMutableArray]()
             
+            print(snapshot)
+            
             for item in snapshot.children {
-                if (item.value["dateInt"] as! Int != 2016) {
+                //if (item.value["dateInt"] as! Int != 2016) {
                 
                     let event = NSMutableArray()
                 
                     let name = String(item.value["name"] as! String)
                     let key = String(item.key as! String)
+                    let date = String(item.value["date"] as! String)
                 
                     event.addObject(name)
                     event.addObject(key)
+                    event.addObject(date)
                     tempEvents.append(event)
-                }
+                //}
             }
-            events = tempEvents
+            if (tempEvents.count > 0) {self.events.addObject(tempEvents)}
+            //self.events = tempEvents
             self.tableView.reloadData()
-            print(events.count)
+            print("query")
+            print(self.events.count)
         
         })
+            
+        }
         
+        //self.tableView.reloadData()
         
         tableView.frame         =   CGRectMake(0, 50, 320, 500);
         tableView.delegate      =   self
@@ -68,14 +92,40 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        //return events.count
+        //if (section == 0) {print("here"); return 2}
+        //if (section == 1) {print("here2"); return events.count-2}
+        //return 1
+        return self.events.objectAtIndex(section).count
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        
+        //return 2
+        return self.events.count
+        
+    }
+    
+     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.events.objectAtIndex(section).objectAtIndex(0).objectAtIndex(2) as! String
+        
+    }
+    
+    
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        //print(indexPath.row)
+        if (events.count) > 0 {
         
-        cell.textLabel?.text = (events[indexPath.row]).objectAtIndex(0) as! String
+        print(self.events.objectAtIndex(indexPath.section).objectAtIndex(indexPath.row).objectAtIndex(0))
+            
+        //cell.textLabel?.text = (self.events[indexPath.row]).objectAtIndex(0) as! String
+        cell.textLabel?.text = self.events.objectAtIndex(indexPath.section).objectAtIndex(indexPath.row).objectAtIndex(0) as! String
+        }
         
         
         return cell
@@ -88,7 +138,8 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         print(events[indexPath.row].objectAtIndex(1))
         
         //self.performSegueWithIdentifier("UpToEvent", sender: indexPath.row)
-        self.performSegueWithIdentifier("UpToEvent", sender: events[indexPath.row].objectAtIndex(1))
+        //self.performSegueWithIdentifier("UpToEvent", sender: self.events[indexPath.row].objectAtIndex(1))
+        self.performSegueWithIdentifier("UpToEvent", sender: self.events.objectAtIndex(indexPath.section).objectAtIndex(indexPath.row).objectAtIndex(1))
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
