d0903e3f0fa84b5b2c0b2570ccd1715718e58b8b
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-04-21 00:28:10 -0400

    data

--- 333 Project/333 Project/SecondViewController.swift
+++ 333 Project/333 Project/SecondViewController.swift
@@ -15,10 +15,11 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     // Get a reference to our posts
     var ref = Firebase(url:"https://blistering-torch-3510.firebaseio.com/games")
-    var games : NSMutableArray = []
+    //var games : NSMutableArray = []
+    var games = [String]()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         // Attach a closure to read the data at our posts reference
@@ -28,13 +29,37 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 print(error.description)
         })
         
+        
+        /*
         ref.queryOrderedByChild("team").observeEventType(.ChildAdded, withBlock: { snapshot in
+        
+            var tempGames = [String]()
             if let team = snapshot.value["team"] as? String{
                 //print(team)
-                self.games.addObject(team)
+                tempGames.append(team)
+                //self.games.addObject(team)
                 self.tableView.reloadData()
             }
+            self.games = tempGames
+            print(self.games.count)
         })
+ */
+        
+        ref.observeEventType(.Value, withBlock: {snapshot in
+            var tempGames = [String]()
+            
+            for item in snapshot.children {
+                
+                let team = String(item.value["team"] as! String)
+                tempGames.append(team)
+                
+            }
+            self.games = tempGames
+            self.tableView.reloadData()
+            print(self.games.count)
+        
+        })
+        
         
         tableView.frame         =   CGRectMake(0, 50, 320, 200);
         tableView.delegate      =   self
@@ -44,22 +69,27 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         self.view.addSubview(tableView)
         
+        super.viewDidLoad()
+        
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //print(games.count)
         return self.games.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+    
+        var text = self.games[indexPath.row] as! String
         
-        //cell.textLabel?.text = self.items[indexPath.row] as! String
-        cell.textLabel?.text = self.games.objectAtIndex(indexPath.row) as? String
+        //cell.textLabel?.text = self.games[indexPath.row] as! String
+        let startIndex = text.startIndex.advancedBy(5)
+        let textTwo = text.substringFromIndex(startIndex)
         
-        print(cell.textLabel?.text)
+        cell.textLabel?.text = textTwo
+        print(textTwo)
         
         return cell
         
