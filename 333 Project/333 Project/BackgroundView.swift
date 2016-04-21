a9b9a4dd94e62c476f553f02e9116b5d97896f78
Mitch Hamburger <MitchHamburger@nat-tvwna-inside-visitornet101-b-1115.princeton.org>
2016-04-20 22:09:58 -0400

    background + buttons

---
+++ 333 Project/333 Project/BackgroundView.swift
@@ -0,0 +1,55 @@
+//
+//  BackgroundView.swift
+//  333 Project
+//
+//  Created by Mitch Hamburger on 4/20/16.
+//  Copyright © 2016 Divya Mehta. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class BackgroundView: UIView {
+    
+    //Weekly sample data
+    var graphPoints:[Int] = [4, 2, 6, 4, 5, 8, 3]
+    
+    //1 - the properties for the gradient
+    @IBInspectable var startColor: UIColor = UIColor.redColor()
+    @IBInspectable var endColor: UIColor = UIColor.greenColor()
+    
+    override func drawRect(rect: CGRect) {
+        
+        let width = rect.width
+        let height = rect.height
+        
+        //set up background clipping area
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: UIRectCorner.AllCorners,
+                                cornerRadii: CGSize(width: 8.0, height: 8.0))
+        path.addClip()
+        
+        //2 - get the current context
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.CGColor, endColor.CGColor]
+        
+        //3 - set up the color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        //4 - set up the color stops
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+        //5 - create the gradient
+        let gradient = CGGradientCreateWithColors(colorSpace,
+                                                  colors,
+                                                  colorLocations)
+        
+        //6 - draw the gradient
+        var startPoint = CGPoint.zero
+        var endPoint = CGPoint(x:0, y:self.bounds.height)
+        CGContextDrawLinearGradient(context,
+                                    gradient,
+                                    startPoint,
+                                    endPoint,
+                                    [])
+}
+}
---
+++ 333 Project/333 Project/TicketButton.swift
@@ -0,0 +1,72 @@
+//
+//  TicketButton.swift
+//  333 Project
+//
+//  Created by Mitch Hamburger on 4/20/16.
+//  Copyright © 2016 Divya Mehta. All rights reserved.
+//
+
+
+import UIKit
+@IBDesignable
+
+class TicketButton: UIButton {
+    @IBInspectable var fillColor: UIColor = UIColor.darkGrayColor()
+    @IBInspectable var strokeColor: UIColor = UIColor.orangeColor()
+    @IBInspectable var isBuy: Bool = false
+    @IBInspectable var isSell: Bool = false
+    
+    
+    override func drawRect(rect: CGRect) {
+        if (isBuy) {
+            let circlePath = UIBezierPath(ovalInRect: rect)
+            /*let path = UIBezierPath(ovalInRect: rect)
+             fillColor.setFill()
+             fillColor.setStroke()
+             path.fill()
+             path.stroke()*/
+            
+            fillColor.setFill();
+            circlePath.fill()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = circlePath.CGPath
+            
+            //change the fill color
+            shapeLayer.fillColor = UIColor.clearColor().CGColor
+            //you can change the stroke color
+            shapeLayer.strokeColor = strokeColor.CGColor
+            //you can change the line width
+            shapeLayer.lineWidth = 3.0
+            
+            super.setTitle("Buy", forState: UIControlState.Normal)
+            super.layer.addSublayer(shapeLayer)
+            super.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+        if (isSell) {
+            let circlePath = UIBezierPath(ovalInRect: rect)
+            /*let path = UIBezierPath(ovalInRect: rect)
+             fillColor.setFill()
+             fillColor.setStroke()
+             path.fill()
+             path.stroke()*/
+            
+            fillColor.setFill();
+            circlePath.fill()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = circlePath.CGPath
+            
+            //change the fill color
+            shapeLayer.fillColor = UIColor.clearColor().CGColor
+            //you can change the stroke color
+            shapeLayer.strokeColor = strokeColor.CGColor
+            //you can change the line width
+            shapeLayer.lineWidth = 3.0
+            
+            super.setTitle("Sell", forState: UIControlState.Normal)
+            super.layer.addSublayer(shapeLayer)
+            super.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+    }
+}
