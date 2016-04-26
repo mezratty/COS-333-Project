674f0744454b2df42a9e36faa817113604bc0143
Alexa Wojak <alexawojak@nat-oitwireless-inside-vapornet100-a-3912.Princeton.EDU>
2016-04-26 19:34:07 -0400

    matching

--- 333 Project/333 Project/FormController.swift
+++ 333 Project/333 Project/FormController.swift
@@ -52,70 +52,47 @@ class FormController: UIViewController {
         
         let urlStringTwo = "https://blistering-torch-3510.firebaseio.com/" + (data.objectAtIndex(2) as! String)
         let refTwo = Firebase(url:urlStringTwo)
-        var counter = 0
         var key = ""
         var matchLast = ""
         var matchFirst = ""
         var matchNetId = ""
-        var matchEventId = data.objectAtIndex(0) as! String
+        let matchEventId = data.objectAtIndex(0) as! String
         
-    refTwo.queryOrderedByChild("eventId").queryEqualToValue(matchEventId).queryLimitedToFirst(1).observeEventType(.Value, withBlock: {snapshot in
-        if (snapshot.exists()) {
-            for item in snapshot.children {
-                key = item.key
-                matchLast = item.value["last"] as! String
-                matchFirst = item.value["first"] as! String
-                matchNetId = item.value["user"] as! String
-                //remove item
-                //add pair to match table
-            }
-        }
-        else {
+    refTwo.queryOrderedByChild("eventId").queryEqualToValue(matchEventId).queryLimitedToFirst(1).observeSingleEventOfType(.Value, withBlock: {snapshot in
+        if (!snapshot.exists()) {
             let urlString = "https://blistering-torch-3510.firebaseio.com/" + (self.data.objectAtIndex(1) as! String)
             let ref = Firebase(url:urlString)
             
             let first = self.firstName.text as! AnyObject
             let last = self.lastName.text as! AnyObject
             
-            let ticket = ["eventId":self.data.objectAtIndex(0) as! String, "user": globalNetId, "first": first, "last": last]
+            let ticket = ["eventId":matchEventId, "user": globalNetId, "first": first, "last": last]
             let ticketRef = ref.childByAutoId()
             ticketRef.setValue(ticket)
-        }
-    })
-        /*
-        
-        if (counter > 1) {
-            counter = 0
-            refTwo.queryOrderedByChild("eventId").queryEqualToValue(matchEventId).observeEventType(.Value, withBlock: {snapshot in
-                if (counter == 0) {
-                    key = snapshot.key
-                    matchLast = snapshot.value["last"] as! String
-                    matchFirst = snapshot.value["first"] as! String
-                    matchNetId = snapshot.value["user"] as! String
-                }
-                counter += 1
-            })
-            
-            print(matchNetId)
             
         }
-        
         else {
-        
-            let urlString = "https://blistering-torch-3510.firebaseio.com/" + (data.objectAtIndex(1) as! String)
-            let ref = Firebase(url:urlString)
-        
-            let first = firstName.text as! AnyObject
-            let last = lastName.text as! AnyObject
-        
-            let ticket = ["eventId":data.objectAtIndex(0) as! String, "user": globalNetId, "first": first, "last": last]
-            let ticketRef = ref.childByAutoId()
-            ticketRef.setValue(ticket)
-        
+            for item in snapshot.children {
+                key = item.key
+                matchLast = item.value["last"] as! String
+                matchFirst = item.value["first"] as! String
+                matchNetId = item.value["user"] as! String
+                refTwo.childByAppendingPath(key).removeValue()
+                
+                let urlString = "https://blistering-torch-3510.firebaseio.com/matches"
+                let ref = Firebase(url:urlString)
+                
+                let first = self.firstName.text as! AnyObject
+                let last = self.lastName.text as! AnyObject
+                
+                let ticket = ["eventId":matchEventId, "user1": globalNetId, "first1": first, "last1": last, "user2": matchNetId, "first2": matchFirst, "last2": matchLast]
+                let ticketRef = ref.childByAutoId()
+                ticketRef.setValue(ticket)
+            }
+            
         }
         
-        
-        */
+    })
     }
 
 }
